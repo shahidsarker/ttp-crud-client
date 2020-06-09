99a76b56ca65bceb0be00de4fc0517afe4a87d0e
Shahid <shahid.sarker@gmail.com>
2020-06-09 13:19:39 -0400

    Set up app with fake Players array

--- src/store/utilities/allPlayers.js
+++ src/store/utilities/allPlayers.js
@@ -1,24 +1,44 @@
-import axios from 'axios';
+import axios from "axios";
 
 // ACTION TYPES;
 const FETCH_ALL_PLAYERS = "FETCH_ALL_PLAYERS";
 
+const allPlayers = [
+  {
+    firstName: "Kyrie",
+    lastName: "Irving",
+    jerseyNumber: 11,
+  },
+  {
+    firstName: "LeBron",
+    lastName: "James",
+    jerseyNumber: 23,
+  },
+  {
+    firstName: "Luka",
+    lastName: "Doncic",
+    jerseyNumber: 77,
+  },
+];
+
 // ACTION CREATORS;
-const fetchAllPlayers = players => {
+const fetchAllPlayers = (players) => {
   return {
     type: FETCH_ALL_PLAYERS,
-    payload: players
-  }
-}
+    payload: players,
+  };
+};
 
 // THUNK CREATORS;
-export const fetchAllPlayersThunk = () => dispatch => {
-  return axios
-    .get('/api/players')
-    .then(res => res.data)
-    .then(players => dispatch(fetchAllPlayers(players)))
-    .catch(err => console.log(err))
-}
+export const fetchAllPlayersThunk = () => (dispatch) => {
+  // return axios
+  //   .get("/api/players")
+  //   .then((res) => res.data)
+  //   .then((players) => dispatch(fetchAllPlayers(players)))
+  //   .catch((err) => console.log(err));
+
+  return dispatch(fetchAllPlayers(allPlayers));
+};
 
 // REDUCER;
 const reducer = (state = [], action) => {
@@ -28,6 +48,6 @@ const reducer = (state = [], action) => {
     default:
       return state;
   }
-}
+};
 
 export default reducer;
