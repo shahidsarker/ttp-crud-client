f27e465c7c86d8ab36ddae994c32303e3ed8eb46
Shahid <shahid.sarker@gmail.com>
2020-06-09 13:41:58 -0400

    Refactor views for Campuses instead of Players;

---
+++ src/components/views/AllCampusesView.js
@@ -0,0 +1,21 @@
+import React from "react";
+// import './styles/AllPlayersView.css';
+import PropTypes from "prop-types";
+
+const AllCampusesView = (props) => {
+  return (
+    <div className="all-players">
+      {props.allCampuses.map((campus) => (
+        <div key={campus.id}>
+          <h1>{campus.name}</h1>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+AllCampusesView.propTypes = {
+  allCampuses: PropTypes.array.isRequired,
+};
+
+export default AllCampusesView;
--- src/thunks/index.js
+++ src/thunks/index.js
@@ -1,2 +1,2 @@
 // Barrel file for thunks, which will be mapped to and invoked within our smart containers;
-export * from '../store/utilities/allPlayers';
+export * from "../store/utilities/allCampuses";
