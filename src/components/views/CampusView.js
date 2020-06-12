c19723ad839dce5a73a324629a6593cddeda343b
Shahid <shahid.sarker@gmail.com>
2020-06-12 10:45:58 -0400

    Display message if campus has no students

--- src/components/views/CampusView.js
+++ src/components/views/CampusView.js
@@ -12,12 +12,19 @@ import React from "react";
  */
 
 const CampusView = (props) => {
+  let studentDisplay;
+  if (props.campus.students) {
+    studentDisplay = <p>{props.campus.students} Students</p>;
+  } else {
+    studentDisplay = <p>There are no students enrolled</p>;
+  }
+
   return (
     <>
       <h1>{props.campus.name}</h1>
       <h3>{props.campus.address}</h3>
       <p>{props.campus.description}</p>
-      <p>{props.campus.students} Students</p>
+      {studentDisplay}
     </>
   );
 };
